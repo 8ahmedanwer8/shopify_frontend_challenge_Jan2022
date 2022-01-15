63f0377e5e3a63264d720147c9dcb1800cca665b
8ahmedanwer8 <84689304+8ahmedanwer8@users.noreply.github.com>
2022-01-15 03:26:49 -0500

    adding all files

---
+++ index.js
@@ -0,0 +1,72 @@
+var body = document.getElementById("body");
+var gui = document.getElementById("gui");
+
+
+
+async function getPics(){
+	carouselDiv = $(".carousel-inner")
+	$(".loading-bg").show()
+	let KEY = "k7sAgi51exUYKYqiFSIRxYMi28Ppy0agK6oBrDcO"
+	url = ""
+	url = url.concat("https://api.nasa.gov/planetary/apod?api_key="+KEY+"&count=10")
+	let response = await fetch(url)
+	console.log(response)
+	let data = await response.json()
+	console.log(data)
+	$(".loading-bg").hide()
+	usePics(data)
+}     
+function pulse() {
+    $('.heart').animate({
+        fontSize: '30px', 
+    }, 150, function() {
+        $('.heart').animate({
+            fontSize: '20px', 
+        }, 120, function() {
+            console.log("animation finished")
+        });
+    }); 
+};
+
+
+function usePics(data){
+	element = ""
+	carouselDiv = $(".carousel-inner")
+	for (i = 0; i < data.length; i++){
+		const src = data[i].url
+		const title = data[i].title
+		const date = data[i].date
+		const explanation = data[i].explanation
+		$(carouselDiv.children()[i].firstElementChild).attr('src', src)
+		$(carouselDiv.children().eq(i).children().eq(1).children())[1].textContent = title
+		$(carouselDiv.children().eq(i).children().eq(1).children())[2].textContent = date
+		$(carouselDiv.children().eq(i).children().eq(1).children())[3].textContent = explanation
+
+	}
+}
+
+(function ($) {
+
+	$( "img" ).hover(function() {
+	  console.log($(this).innerHTML)
+	});
+
+	$(document).ready(function() {
+		getPics()	
+	});
+
+	$(".heart").click(function(){
+		if ($(this).hasClass("like")){
+			$(this).removeClass("like")
+		}
+		else{
+			pulse();
+			$(this).addClass("like")
+		}
+
+
+
+	})
+
+})(jQuery);
+
